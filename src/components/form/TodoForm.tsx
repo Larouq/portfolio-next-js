968d886b91fff21a028e6fbf31a6ae2a39deeca2
Larouq <rouquie.b@gmail.com>
2023-10-24 10:58:17 +0200

    add styles input

--- src/components/form/TodoForm.tsx
+++ src/components/form/TodoForm.tsx
@@ -3,10 +3,12 @@
 import { Button } from "../button";
 import { Plus } from "phosphor-react";
 
+import styles from "./todoForm.module.scss";
+
 export const TodoForm: React.FC = () => {
   return (
-    <form>
-      <input />
+    <form className={styles.todoForm_container}>
+      <input className={styles.form_input} />
       <Button
         variant="primary"
         text="Add"
