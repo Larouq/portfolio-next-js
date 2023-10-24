9bfdf5575a86c513c8e31cbf2736fdf1ee02c713
Larouq <rouquie.b@gmail.com>
2023-10-24 19:10:16 +0200

    fix lint

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ReactNode } from "react";
+import { experimental_useFormState as useFormStatus } from "react-dom";
 import classNames from "classnames";
 
 import styles from "./button.module.scss";
@@ -22,12 +23,21 @@ export const Button: React.FC<ButtonProps> = ({
   icon,
   disabled,
 }) => {
+  const { pending } = useFormStatus();
+
   const buttonClassNames = classNames(styles.Button, {
     [styles[`button-${variant}`]]: variant,
+    [styles["button-disabled"]]: disabled || pending,
   });
 
+  console.log("pending", pending);
+
   return (
-    <button className={buttonClassNames} onClick={onClick} disabled={disabled}>
+    <button
+      className={buttonClassNames}
+      onClick={onClick}
+      disabled={disabled || pending}
+    >
       {icon}
       <span>{text}</span>
     </button>
